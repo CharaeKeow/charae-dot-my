ff889ed4f6d3dfa577a3c06e26f95a553c88eaea
charaekeow <charaekeow11@gmail.com>
2024-11-10 00:37:38 +0800

    feat: add @svgr/webpack setup

---
+++ next.config.js
@@ -0,0 +1,35 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  webpack(config) {
+    // Grab the existing rules that handles SVG import
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // Exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+};
+
+export default nextConfig;
