ec5315a06f53e65339d710eae52ffafcf9cf47d6
charaekeow <charaekeow11@gmail.com>
2024-11-10 17:02:59 +0800

    feat: add my toolbelts section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Bio from '@/features/home/components/Bio';
+import Toolbelt from '@/features/home/components/Toolbelt';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 font-[family-name:var(--font-geist-sans)] sm:p-20">
+    <div className="flex h-full flex-col justify-start gap-y-9 p-4 font-[family-name:var(--font-geist-sans)] lg:p-8">
       <Bio />
+      <Toolbelt />
     </div>
   );
 }
