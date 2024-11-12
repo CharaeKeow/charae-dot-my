4c3c8b219e29ba4d2e5e12806658bd0723b3fdb3
charaekeow <charaekeow11@gmail.com>
2024-11-12 14:47:07 +0800

    refactor: extract types out to feature types folder file

--- src/features/home/types/index.ts
+++ src/features/home/types/index.ts
@@ -1,6 +1,18 @@
-import { SOCIALS_SITE } from '../constants';
+import type { SOCIALS_SITE } from '../constants';
+import type { StaticImageData } from 'next/image';
 
 export type Socials = {
   name: keyof typeof SOCIALS_SITE;
   link: string;
+};
+
+export type WorkExperienceData = {
+  image: StaticImageData;
+  jobTitle: string;
+  company: string;
+  companyLink: string;
+  date: string;
+  description: string;
+  techStack: string[];
+  otherStack?: string[];
 };
